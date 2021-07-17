bebb4d04f085961d367eed86d8ea2e3cd0a130ad
ronothnay <ron.othnay@gmail.com>
2021-07-17 08:15:13 +0300

    added FK to several tables, design diagram

--- sql_objects/tables/characters_in_episodes.sql
+++ sql_objects/tables/characters_in_episodes.sql
@@ -1,6 +1,11 @@
 CREATE TABLE IF NOT EXISTS storefirst.characters_in_episodes
 (
-    location_id character(50) NOT NULL,
+    episode_id character(50) NOT NULL,
     character_id character(50) NOT NULL,
-    insertion_time date NOT NULL DEFAULT CURRENT_DATE
+    insertion_time date NOT NULL DEFAULT CURRENT_DATE,
+    PRIMARY KEY (episode_id, character_id)
+    CONSTRAINT fk_location FOREIGN KEY (episode_id)
+        REFERENCES storefirst.episodes (episode_id) MATCH SIMPLE,
+    CONSTRAINT fk_character FOREIGN KEY (character_id)
+        REFERENCES storefirst.characters (character_id) MATCH SIMPLE
 );
--- sql_objects/tables/residents_in_locations.sql
+++ sql_objects/tables/residents_in_locations.sql
@@ -2,5 +2,10 @@ CREATE TABLE IF NOT EXISTS storefirst.residents_in_locations
 (
     location_id character(50) NOT NULL,
     character_id character(50) NOT NULL,
-    insertion_time date NOT NULL DEFAULT CURRENT_DATE
+    insertion_time date NOT NULL DEFAULT CURRENT_DATE,
+    PRIMARY KEY (location_id, character_id)
+    CONSTRAINT fk_location FOREIGN KEY (location_id)
+        REFERENCES storefirst.locations (location_id) MATCH SIMPLE,
+    CONSTRAINT fk_character FOREIGN KEY (character_id)
+        REFERENCES storefirst.characters (character_id) MATCH SIMPLE
 );
